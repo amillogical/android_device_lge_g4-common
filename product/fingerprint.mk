c7fea65c8733bf9947e690b228d88c732f8ab49b
RealJohnGalt <johngalt@fake.mail>
2018-04-14 13:46:28 -0700

    Revert "custom fingerprint hal:"
    This reverts commit 57ba80b515a7e05bc577bbbd2fb18bcb1be541ef.

--- product/fingerprint.mk
+++ product/fingerprint.mk
@@ -1,4 +1,2 @@
 PRODUCT_PACKAGES += \
-		    android.hardware.biometrics.fingerprint@2.0-service-custom
-PRODUCT_COPY_FILES += \
-		      frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
+		    android.hardware.biometrics.fingerprint@2.0-service
