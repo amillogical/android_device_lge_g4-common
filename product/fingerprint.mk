57ba80b515a7e05bc577bbbd2fb18bcb1be541ef
RealJohnGalt <johngalt@fake.mail>
2018-04-14 12:26:49 -0700

    custom fingerprint hal:
    from https://github.com/alexstsv2/biometrics

--- product/fingerprint.mk
+++ product/fingerprint.mk
@@ -1,2 +1,4 @@
 PRODUCT_PACKAGES += \
-		    android.hardware.biometrics.fingerprint@2.0-service
+		    android.hardware.biometrics.fingerprint@2.0-service-custom
+PRODUCT_COPY_FILES += \
+		      frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
