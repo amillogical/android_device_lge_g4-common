52bee320da38d87c95ab98bca8bb90939b4286ea
RealJohnGalt <johngalt@fake.mail>
2018-05-02 11:45:24 -0700

    fingerprint bringup: 1.0 HIDL HAL

---
+++ product/fingerprint.mk
@@ -0,0 +1,5 @@
+PRODUCT_PACKAGES += \
+		    android.hardware.biometrics.fingerprint@2.0-service.v10
+
+PRODUCT_COPY_FILES += \
+		    frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
