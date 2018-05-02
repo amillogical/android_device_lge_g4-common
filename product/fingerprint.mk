4744161805afe2f2a4c90ef0fe7a1a14e6f292a9
RealJohnGalt <johngalt@fake.mail>
2018-05-02 14:21:00 -0700

    fingerprint: bump to 2.1

--- product/fingerprint.mk
+++ product/fingerprint.mk
@@ -1,5 +1,5 @@
 PRODUCT_PACKAGES += \
-		    android.hardware.biometrics.fingerprint@2.0-service.v10
+		    android.hardware.biometrics.fingerprint@2.1-service.v10
 
 PRODUCT_COPY_FILES += \
 		    frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
