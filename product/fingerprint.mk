9cb5902b071a30a73cbaecfa601d3a7eb2b89dc5
jg <jg@local.lan>
2018-05-07 13:30:51 -0700

    WIP fingerprint HAL

--- product/fingerprint.mk
+++ product/fingerprint.mk
@@ -1,5 +1,6 @@
 PRODUCT_PACKAGES += \
-		    android.hardware.biometrics.fingerprint@2.1-service.v10
+		    android.hardware.biometrics.fingerprint@2.1-service \
+		    fingerprint.msm8992
 
 PRODUCT_COPY_FILES += \
 		    frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
